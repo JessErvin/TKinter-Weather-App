e7691e2f4ddc726d16d75a1a2e2cd41371798882
mmmSprinkles <jemail9@gmail.com>
2019-10-25 14:52:03 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TKWeather',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Jessica Ervin',
+    author_email='',
+    description=''
+)
